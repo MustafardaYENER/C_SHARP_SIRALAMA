0d4d926be89f479c1f30191bfe449459e0a6ec56
MustafardaYENER <m.ardayener@gmail.com>
2024-08-10 00:05:44 +0300

    Update Program.cs

--- if else sıralama/Program.cs
+++ if else sıralama/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            #region if else sıralama
+                
             Console.WriteLine("1.Sayıyı Giriniz.");
             int sayi1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("2.Sayıyı Giriniz");
@@ -66,9 +68,26 @@
             {
                Console.WriteLine("Hatalı sayı girildi lütfen tekrar sayı giriniz!!!");
             }
-            
+            #endregion
 
+              #region yazılan sayıların toplamı ve ortalaması
+   Console.WriteLine("Sayı giriniz:");
+   int sayi = Convert.ToInt32(Console.ReadLine());
+   int toplam = sayi;
+   int i = 0, sayac = 1;
 
+   while (sayi != 0)
+   {
+       Console.WriteLine("Sayı giriniz:");
+       sayi = Convert.ToInt32(Console.ReadLine());
+       toplam += sayi;
+       if (sayi != 0)
+           sayac++;
+   }
+
+   Console.WriteLine("Girilen sayıların toplamı={0} Ortalaması={1}", toplam, toplam / sayac);
+   Console.ReadKey();
+   #endregion
 
 
 
